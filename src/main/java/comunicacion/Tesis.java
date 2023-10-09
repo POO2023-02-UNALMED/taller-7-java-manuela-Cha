96acdf5b486e4257bef5456ca520bfcab5dec93a
manuela-Cha <141794878+manuela-Cha@users.noreply.github.com>
2023-10-08 22:20:13 -0500

    Create Tesis.java

---
+++ src/main/java/comunicacion/Tesis.java
@@ -0,0 +1,47 @@
+package comunicacion;
+
+public class Tesis extends Escrito{
+    private String idea;
+    private String[] argumentos;
+    private String conclusion;
+    private String referencias;
+    private String interpretacion;
+
+    public Tesis(String origen, String titulo, String autor, int paginas, String idea, String[] argumentos, String conclusion, String referencias, String interpretacion){
+        super(origen, titulo, autor, paginas);
+        this.idea = idea;
+        this.argumentos = argumentos;
+        this.conclusion = conclusion;
+        this.referencias = referencias;
+        this.interpretacion = interpretacion;
+    }
+
+    public String getIdea(){
+        return idea;
+    }
+
+    public String[] getArgumentos(){
+        return argumentos;
+    }
+
+    public String getConclusion(){
+        return conclusion;
+    }
+
+    public String getReferencias(){
+        return referencias;
+    }
+
+    String interpretacion(){
+        return interpretacion;
+    }
+
+    public String toString(){
+        return super.getOrigen() + "\n" + super.getTitulo() + "\n" + super.getAutor() + "\n" + super.getPaginas() + "\n" + this.getIdea() + "\n" + this.getArgumentos() + "\n" + this.getConclusion() + "\n" + this.getReferencias();
+    }
+
+    int palabrasTotales(int palabrasPagina){
+        int factor = 5;
+        return palabrasPagina * factor * super.getPaginas();
+    }
+}
