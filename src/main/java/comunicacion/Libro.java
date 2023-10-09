ef1a4b3188000b5a367d6c5cce25932744cd9cea
manuela-Cha <141794878+manuela-Cha@users.noreply.github.com>
2023-10-08 22:18:36 -0500

    Create Libro.java

---
+++ src/main/java/comunicacion/Libro.java
@@ -0,0 +1,42 @@
+package comunicacion;
+
+public class Libro extends Escrito{
+    private String co_autor;
+    private String editorial;
+    private String edicion;
+    private String interpretacion;
+
+
+    public Libro(String origen, String titulo, String autor, int paginas, String co_autor, String editorial, String edicion, String interpretacion){
+        super(origen, titulo, autor, paginas);
+        this.co_autor = co_autor;
+        this.editorial = editorial;
+        this.edicion = edicion;
+        this.interpretacion = interpretacion;
+    } 
+
+    public String getCo_autor (){
+        return co_autor;
+    }
+
+    public String getEditorial(){
+        return editorial;
+    }
+
+    public String getEdicion(){
+        return edicion;
+    }
+
+    String interpretacion(){
+        return interpretacion;
+    }
+
+    public String toString(){
+        return super.getOrigen() + "\n" + super.getTitulo() + "\n" + super.getAutor() + "\n" + super.getPaginas() + "\n" + this.getCo_autor() + "\n" + this.getEditorial() + "\n" + this.getEdicion();
+    }
+
+    int palabrasTotales(int palabrasPagina){
+        int factor = 2;
+        return palabrasPagina * factor * super.getPaginas();
+    }   
+}
