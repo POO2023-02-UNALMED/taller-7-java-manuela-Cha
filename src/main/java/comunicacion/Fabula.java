5052e0e6b982daf5440fc08ed3e56f0b03b74316
manuela-Cha <141794878+manuela-Cha@users.noreply.github.com>
2023-10-08 22:26:57 -0500

    Update Fabula.java

--- src/main/java/comunicacion/Fabula.java
+++ src/main/java/comunicacion/Fabula.java
@@ -4,9 +4,9 @@ public class Fabula extends Escrito {
     private String ensenanza;
     private String interpretacion;
 
-    public Fabula(String origen, String titulo, String autor, int paginas, String enseñanza, String interpretacion){
+    public Fabula(String origen, String titulo, String autor, int paginas, String ensenanza, String interpretacion){
         super(origen, titulo, autor, paginas);
-        this.ensenanza = enseñanza;
+        this.ensenanza = ensenanza;
         this.interpretacion = interpretacion; 
     }
 
